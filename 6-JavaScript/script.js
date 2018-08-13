c47ebd542426339811137775b24b51dd798b8731
gekif <dzulfikar.maulana@gmail.com>
2018-08-13 15:08:56 +0700

    Mini Facebook

--- 6-JavaScript/script.js
+++ 6-JavaScript/script.js
@@ -1,26 +1,31 @@
-function sayHello() {
-    console.log('Hellow');
+var database = [
+    {
+        username: "Fikar",
+        password: "12345"
+    }
+];
+
+var newsFeed = [
+    {
+        username: "Anjay",
+        timeline: "tired from all that learning"
+    },
+    {
+        username: "Sally",
+        timeline: "Javascipt is cool"
+    }
+];
+
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("Enter your password");
+
+function singIn(user, pass) {
+    if (user === database[0].username &&
+        pass === database[0].password) {
+        console.log(newsFeed);
+    } else {
+        alert("Incorrect");
+    }
 }
 
-//Function expression
-var sayBye = function () {
-    console.log('goodbye');
-};
-
-var sing = function (sing) {
-    console.log("i'm singing " + sing);
-};
-
-
-
-input.onclick = function () {
-    var a = '';
-    var b = prompt("Choose your song" + a);
-    sing(b);
-};
-
-button.onclick = function () {
-    sayHello();
-    sayBye();
-};
-
+singIn(userNamePrompt, passwordPrompt);
