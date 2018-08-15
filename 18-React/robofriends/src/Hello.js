86c4843a98e77e87773512e3637428b44315a27f
gekif <dzulfikar.maulana@gmail.com>
2018-08-15 13:53:24 +0700

    First React Component of Robofriends

---
+++ 18-React/robofriends/src/Hello.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import './Hello.css';
+
+// This is object
+/*class Hello extends Component {
+    render() {
+        return (
+            <div className="f1 tc">
+                <h1>Hello World</h1>
+                <p>{this.props.greeting}</p>
+            </div>
+        );
+    }
+}*/
+
+// This is function
+const Hello = (props) => {
+    return (
+        <div className="f1 tc">
+            <h1>Hello World</h1>
+            <p>{props.greeting}</p>
+        </div>
+    );
+};
+
+export default Hello;
