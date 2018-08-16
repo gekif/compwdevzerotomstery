5008c52e9fcfa531dc4742118c08985f19d70f19
gekif <dzulfikar.maulana@gmail.com>
2018-08-16 09:01:31 +0700

    Robofriends App Version 6.1

--- 18-React/robofriends/src/containers/App.js
+++ 18-React/robofriends/src/containers/App.js
@@ -25,14 +25,14 @@ class App extends Component {
     };
 
     render() {
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state;
+         const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         });
 
-        if (this.state.robots.length === 0) {
-            return <h1 className="tc">Loading</h1>;
-        } else {
-            return (
+        return !robots.length ?
+            <h1 className="tc">Loading</h1> :
+             (
                 <div className="tc">
                     <h1 className="f2">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
@@ -41,10 +41,8 @@ class App extends Component {
                     </Scroll>
                 </div>
             );
-        }
-
-
     }
+
 }
 
 
