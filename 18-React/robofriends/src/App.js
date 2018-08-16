c9f7e7c24b0bbda0bff5e2df97f8f8bfa4c840e2
gekif <dzulfikar.maulana@gmail.com>
2018-08-16 08:36:58 +0700

    Robofriends App Version 4

--- 18-React/robofriends/src/App.js
+++ 18-React/robofriends/src/App.js
@@ -9,9 +9,15 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}));
     }
 
     onSearchChange = (event) => {
@@ -23,13 +29,19 @@ class App extends Component {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         });
 
-        return (
-            <div className="tc">
-                <h1 className="f2">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots}/>
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1 className="tc">Loading</h1>;
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f2">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
+        }
+
+
     }
 }
 
