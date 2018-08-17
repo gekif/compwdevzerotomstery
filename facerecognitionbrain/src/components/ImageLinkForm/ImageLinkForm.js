4f0e8f3edfe93f7252f3d549a1aed8fcfd3376f9
gekif <dzulfikar.maulana@gmail.com>
2018-08-17 10:11:01 +0700

    Robofriends App Version 9.1

--- facerecognitionbrain/src/components/ImageLinkForm/ImageLinkForm.js
+++ facerecognitionbrain/src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,15 +5,15 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div>
            <p className="f3">
-               {'This Magic Brain will detect faces in your pictures. Give it try'}
+               {'Masukan URL Gambar, Maka Akan Muncul Gambar Lhooo'}
            </p>
             <div className="center">
                 <div className="form center pa4 br3 shadow-5">
-                    <input className="f4 pa2 w-70 center" type="text" onChange={onInputChange}/>
+                    <input className="f4 pa2 w-70 center ip" type="text" onChange={onInputChange}/>
                     <button
-                        className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+                        className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple btn"
                         onClick={onButtonSubmit}
-                    >Detect</button>
+                    >Open</button>
                 </div>
             </div>
         </div>
