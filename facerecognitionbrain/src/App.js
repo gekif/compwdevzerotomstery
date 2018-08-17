7fe4054bdd51d33a2d4933fbb51b3343180c8490
gekif <dzulfikar.maulana@gmail.com>
2018-08-17 07:59:00 +0700

    Robofriends App Version 7

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -3,8 +3,16 @@ import Navigation from './components/Navigation/Navigation';
 import Particles from 'react-particles-js';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
 import './App.css';
+import Clarifai from 'clarifai';
+
+
+const app = new Clarifai.App({
+    apiKey: 'YOUR_API_KEY'
+});
+
 
 const particleOptions = {
     particles: {
@@ -18,7 +26,35 @@ const particleOptions = {
     }
 };
 
+
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            input: '',
+            imageUrl: ''
+        }
+    }
+
+    onInputChange = (event) => {
+        this.setState({input: event.target.value});
+    };
+
+    onButtonSubmit = () => {
+        this.setState({imageUrl: this.state.input});
+        app.models.predict(
+            Clarifai.FACE_DETECT_MODEL,
+            this.state.input)
+            .then(
+            function(response) {
+                console.log(response.output[0].data.regions[0].region_info.bounding_box);
+            },
+            function (err) {
+                
+            }
+        );
+    };
+
     render() {
         return (
             <div className="App">
@@ -28,11 +64,15 @@ class App extends Component {
                 <Navigation/>
                 <Logo />
                 <Rank />
-                <ImageLinkForm />
-                {/*<FaceRecognition /> */}
+                <ImageLinkForm
+                    onInputChange = {this.onInputChange}
+                    onButtonSubmit = {this.onButtonSubmit}
+                />
+                <FaceRecognition imageUrl={this.state.imageUrl} />
             </div>
         );
     }
 }
+
 
 export default App;
