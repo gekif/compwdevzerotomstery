85fd51554df9030e375752bd52a7fc8ed71a3931
gekif <dzulfikar.maulana@gmail.com>
2018-08-17 22:23:39 +0700

    App backend 4

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -37,9 +37,26 @@ class App extends Component {
             imageUrl: '',
             box: {},
             route: 'signin',
-            isSignedIn: false
+            isSignedIn: false,
+            user: {
+                id: '',
+                name: '',
+                email: '',
+                entries: 0,
+                joined: ''
+            }
         }
     }
+
+    loadUser = (data) => {
+        this.setState({user: {
+            id: data.id,
+                name: data.name,
+                email: data.email ,
+                entries: data.entries,
+                joined: data.joined
+        }})
+    };
 
     calculateFaceLocation = (data) => {
         const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -104,7 +121,7 @@ class App extends Component {
                     : (
                         route === 'signin'
                             ? <Signin onRouteChange={this.onRouteChange} />
-                            : <Register onRouteChange={this.onRouteChange} />
+                            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
                       )
                 }
                 </div>
