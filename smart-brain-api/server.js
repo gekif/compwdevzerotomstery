bee42519478a7b688403fd619acf53647dae0ff2
gekif <dzulfikar.maulana@gmail.com>
2018-08-19 06:30:35 +0700

    App Database 3

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -80,27 +80,32 @@ app.post('/signin', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    db('users').insert({
-        email: email,
-        name: name,
-        joined: new Date()
-    }).then(console.log);
-    res.json(database.users[database.users.length - 1]);
+    const { email, name } = req.body;
+    db('users')
+        .returning('*')
+        .insert({
+            email: email,
+            name: name,
+            joined: new Date()
+    })
+        .then(user => {
+            res.json(user[0]);
+        })
+        .catch(err => res.status(400).json('unable to register'));
 });
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
     let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-           return res.json(user);
-        }
-    });
-    if (!found) {
-        res.status(400).json('not found');
-    }
+    db.select('*').from('users').where({id})
+        .then(user => {
+            if (user.length) {
+                res.json(user[0])
+            } else {
+                res.status(400).json('not found');
+            }
+    })
+        .catch(err => res.status(400).json('error getting user'));
 });
 
 app.put('/image', (req, res) => {
