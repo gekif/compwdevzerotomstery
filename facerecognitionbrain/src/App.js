61dfcef57669ceb39610dca8d75dfcc577293cb7
gekif <dzulfikar.maulana@gmail.com>
2018-08-19 07:18:50 +0700

    App Database 6

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -28,24 +28,25 @@ const particleOptions = {
     }
 };
 
+const initialState = {
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signin',
+    isSignedIn: false,
+    user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+    }
+};
 
 class App extends Component {
     constructor() {
         super();
-        this.state = {
-            input: '',
-            imageUrl: '',
-            box: {},
-            route: 'signin',
-            isSignedIn: false,
-            user: {
-                id: '',
-                name: '',
-                email: '',
-                entries: 0,
-                joined: ''
-            }
-        }
+        this.state = initialState;
     }
 
     loadUser = (data) => {
